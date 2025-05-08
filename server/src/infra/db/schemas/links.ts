6d3aa64006b343a37cd5e214fac0c30883ab9b01
Augusto Moscardo <augustomsaluz@gmail.com>
2025-05-08 16:34:25 -0300

    apply uuidv7 schema generation

--- server/src/infra/db/schemas/links.ts
+++ server/src/infra/db/schemas/links.ts
@@ -1,10 +1,11 @@
 import { randomUUID } from 'node:crypto'
 import { integer, pgTable, primaryKey, text, timestamp } from 'drizzle-orm/pg-core'
+import { uuidv7 } from 'uuidv7'
 
 export const links = pgTable('links', {
   id: text('id')
     .primaryKey()
-    .$defaultFn(() => randomUUID()),
+    .$defaultFn(() => uuidv7()),
   originalUrl: text('original_url').notNull(),
   shortUrl: text('short_url').notNull().unique(),
   accessCount: integer('access_count').default(0).notNull(),
