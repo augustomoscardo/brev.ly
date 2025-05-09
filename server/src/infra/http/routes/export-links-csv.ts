0fa4c0547f5d46f48ef0b6e91252d63e8d1e7abc
Augusto Moscardo <augustomsaluz@gmail.com>
2025-05-09 16:14:50 -0300

    add validation in export csv with empty links

--- server/src/infra/http/routes/export-links-csv.ts
+++ server/src/infra/http/routes/export-links-csv.ts
@@ -2,9 +2,12 @@ import { randomUUID } from 'node:crypto'
 import { PassThrough } from 'node:stream'
 import { pipeline } from 'node:stream/promises'
 import { env } from '@/env'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
 import { r2 } from '@/infra/storage/client'
 import { Upload } from '@aws-sdk/lib-storage'
 import { stringify } from 'csv-stringify'
+import { count } from 'drizzle-orm'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { Pool } from 'pg'
 import QueryStream from 'pg-query-stream'
@@ -34,25 +37,30 @@ export const exportLinksCsvRoute: FastifyPluginAsyncZod = async app => {
       const pgClient = await pgPool.connect()
 
       try {
+        const [query] = await db.select({ linksCount: count() }).from(schema.links)
+        const { linksCount } = query
+
+        if (linksCount <= 0) {
+          return reply.status(400).send({ message: 'There is no link to be exported' })
+        }
+
         const queryStream = new QueryStream(`
           SELECT  
-            id, "original_url" as originalUrl, "short_url" as shortUrl, "access_count" as accessCount, "created_at" as createdAt 
+            "id", "original_url", "short_url", "access_count", "created_at" 
           FROM links  
         `)
-        console.log(queryStream)
 
         const dbStream = pgClient.query(queryStream)
-        console.log(dbStream)
 
         const csvStream = stringify({
           delimiter: ';',
           header: true,
           columns: [
             { key: 'id', header: 'id' },
-            { key: 'originalUrl', header: 'originalUrl' },
-            { key: 'shortUrl', header: 'shortUrl' },
-            { key: 'accessCount', header: 'accessCount' },
-            { key: 'createdAt', header: 'createdAt' },
+            { key: 'original_url', header: 'originalUrl' },
+            { key: 'short_url', header: 'shortUrl' },
+            { key: 'access_count', header: 'accessCount' },
+            { key: 'created_at', header: 'createdAt' },
           ],
         })
 
@@ -82,9 +90,9 @@ export const exportLinksCsvRoute: FastifyPluginAsyncZod = async app => {
         return reply.status(200).send({ message: 'Links exported successfully', fileUrl })
       } catch (error) {
         pgClient.release()
+        console.log(error)
+
         return reply.status(500).send({ message: 'Internal server error' })
-      } finally {
-        pgClient.release()
       }
     }
   )
