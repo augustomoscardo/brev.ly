51f7b4059f35d1dbd7afb813d004d5b0d737b7a4
Augusto Moscardo <augustomsaluz@gmail.com>
2025-05-16 00:06:19 -0300

    started ui styling

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,8 +1,28 @@
-import { Copy, Link } from "@phosphor-icons/react";
-import logoImg from "../public/assets/Logo.svg"
+import { Copy, DownloadSimple, Link, Trash } from "@phosphor-icons/react";
+import { useEffect, useState } from "react";
+import logoImg from "/assets/Logo.svg"
+import type { LinkObject } from "./@types/link";
+import { api } from "./services/api";
+
+
 
 export function App() {
-  
+  const [links, setLinks] = useState<LinkObject[]>([])
+
+  useEffect(() => {
+    async function fetchLinks() {
+    const response = await api.get<LinkObject[]>('/links')
+
+    if (response.data) return setLinks(response.data)
+
+    setLinks([])
+  }
+
+  fetchLinks()
+  }, [])
+
+  const appUrl = window.location.origin
+
   return (
     <div className="w-full flex flex-col gap-8">
       <header className="w-full">
@@ -37,23 +57,49 @@ export function App() {
             </div>
           </div>
 
-          <button type="button">
+          <button type="button" className="bg-blue-base text-white text-md leading-md rounded-lg py-4 cursor-pointer hover:bg-blue-dark disabled:opacity-50 disabled:cursor-not-allowed">
             Salvar link
           </button>
         </div>
 
-        <div className="flex-2 p-8 bg-gray-100 flex flex-col gap-6 rounded-lg">
-          <div>
-            <h2>Meus links</h2>
-            <button type="button">
-              <Copy size={24} />
+        <div className="flex-2 flex flex-col gap-5 p-8 bg-gray-100 rounded-lg">
+          <div className="flex justify-between items-center">
+            <h2 className="text-lg leading-lg text-gray-600 font-bold">Meus links</h2>
+            <button 
+              type="button" 
+              className={`flex items-center gap-1.5 text-sm leading-sm rounded ${links.length > 0 ? '' : 'disabled:text-gray-500 disabled:bg-transparent'}`}
+              
+            >
+              <DownloadSimple size={16} />
               Baixar CSV
             </button>
           </div>
-          <div>
-            <Link />
-            Ainda não existem links cadastrados
+          { links.length > 0 ? (
+            <div className="flex flex-col py-4 space-y-8">
+              {links.map(link => (
+                <div key={link.id} className="flex gap-5 items-center">
+                  <div className="flex-1 flex flex-col gap-1">
+                    <p className="w-fit text-md leading-md text-blue-base cursor-pointer font-semibold hover:text-blue-dark hover:underline">{`${appUrl}/${link.shortUrl}`}</p>
+                    <p className="text-sm leading-sm text-gray-500">{link.originalUrl}</p>
+                  </div>
+                  <p className="text-xs leading-xs text-gray-500">{link.accessCount} acessos</p>
+                  <div className="flex items-center gap-1">
+                    <button type="button" className="p-2.5 text-gray-600 bg-gray-200 rounded-sm cursor-pointer">
+                      <Copy size={16} />
+                    </button>
+                    <button type="button" className="p-2.5 text-gray-600 bg-gray-200 rounded-sm cursor-pointer">
+                      <Trash size={16} />
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col gap-3 items-center justify-center h-full">
+              <Link size={32} className="text-gray-400" />
+              <p className="text-xs leading-xs text-gray-500 uppercase">Ainda não existem links cadastrados</p>
           </div>
+          )}
         </div>
       </div>
     </div>
